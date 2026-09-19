4d04675c92d14c2d673001ec3e8135ae42bfb238
Charles Goodsir <charlesguni2024@gmail.com>
2026-09-19 14:25:10 +1200

    fixing issues from semgrep.sarif

--- infra/azure-storage/storage_account.tf
+++ infra/azure-storage/storage_account.tf
@@ -4,4 +4,5 @@ resource "azurerm_storage_account" "main" {
   location                 = azurerm_resource_group.main.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  min_tls_version          = "TLS1_2"
 }
